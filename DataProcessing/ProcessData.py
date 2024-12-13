97ad604bd0f67ac49bba1b37f42c7e5652961864
TasmiaH0508 <Tasmiahaque2004@gmail.com>
2024-12-13 14:05:14 +0800

    Working on testing code and SVM

--- DataProcessing/ProcessData.py
+++ DataProcessing/ProcessData.py
@@ -3,11 +3,17 @@ import os
 import numpy as np
 from PIL import Image
 
+import torch
+
 P_folder = "./Raw Data/PNEUMONIA"
 NORMAL_folder_1 = "./Raw Data/NORMAL"
-target_size = (256, 256) # The size (64, 64) can still work for a total of 400 samples
+target_size = (256, 256)
 
 def process_images(image_folder, target_size):
+    ''''''''''
+    Takes a folder of '.png' images and returns a 2D np array, where the rows are the data points and 
+    the columns are the features(ie has shape (num data points, num columns)), each of which maps to a pixel
+    '''''''''''
     image_features = []
     for filename in os.listdir(image_folder):
         if filename.endswith(".png"):
@@ -30,36 +36,35 @@ def process_images(image_folder, target_size):
 
 # Reduce Dimensionality of Data
 
-import torch
-
 def pca(data):
+    '''''''''''
+    Takes a 2D np array and returns a 2D np array, with reduced number of features. The shape of the output 
+    is (num data points, reduced number of features)
+    '''''
     # data has the shape (number of data points, number of features)
     data = data.T
-    print("Data has been transposed.")
+    data = torch.from_numpy(data)
     # data now has the shape (number of features, number of sata points)
     num_data_points = data.shape[1]
-    print(1)
-    mean_matrix = np.mean(data, axis=1, keepdims=True)
-    print(2)
+    mean_matrix = torch.mean(data, dim=0)
     mean_centred_data = data - mean_matrix
-    print(3)
-    covariance_matrix = (mean_centred_data @ mean_centred_data.T) / num_data_points # takes a long time for covariance matrix to be computed -> maybe consider using built in numpy function
-    print("Covariance computed")
+    covariance_matrix = (mean_centred_data @ mean_centred_data.T) / num_data_points
     # perform svd
-    U, S, _ = np.linalg.svd(covariance_matrix)
-    explained_variance_ratio = np.cumsum(S) / np.sum(S)
-    r = np.searchsorted(explained_variance_ratio, 0.99) + 1
+    U, S, _ = torch.linalg.svd(covariance_matrix)
+    explained_variance_ratio = torch.cumsum(S, dim=0) / torch.sum(S)
+    r = torch.searchsorted(explained_variance_ratio, 0.99) + 1
     U_reduced = U[:, : r]
     # reduce data, no reconstruction is performed since the goal here is to reduce dimensions
     Z = U_reduced.T @ data
     Z = Z.T
-    print("Z has been computed")
     # change Z so that it has the same shape as original data: (number of data points, number of features)
     return Z
 
-# utilise batch processing
-
-def pca_with_batch_processing(data, batch_size=10):
+def pca_with_batch_processing(data, batch_size=400):
+    '''''''''
+    Takes a 2D np array and returns a 2D np array, with reduced number of features. The shape of the output 
+    is (num data points, reduced number of features). Is faster for large data sets.
+    '''
     # data has the shape (number of data points, number of features)
     data = data.T
     data = torch.from_numpy(data)
@@ -68,12 +73,10 @@ def pca_with_batch_processing(data, batch_size=10):
     num_data_points = data.shape[1]
     mean_matrix = torch.mean(data, dim=0)
     mean_centred_data = torch.zeros_like(data)
-    print(1)
     # batch processing
     for i in range(0, num_data_points, batch_size):
         batch_end = min(i + batch_size, num_data_points)
         mean_centred_data[:, i:batch_end] = data[:, i:batch_end] - mean_matrix
-    print(2)
     # do batch processing
     covariance_matrix = torch.zeros((num_features, num_features))
     for i in range(0, num_features, batch_size):
@@ -81,13 +84,11 @@ def pca_with_batch_processing(data, batch_size=10):
         batch = mean_centred_data[:, i:batch_end]
         covariance_matrix += batch @ batch.T
     covariance_matrix /= num_data_points
-    print(3)
     # perform svd
     U, S, _ = torch.linalg.svd(covariance_matrix)
     explained_variance_ratio = torch.cumsum(S, dim=0)/ torch.sum(S)
     r = torch.searchsorted(explained_variance_ratio, 0.99) + 1
     U_reduced = U[:, : r].float()
-    print("SVD performed and U reduced")
     # reduce data, no reconstruction is performed since the goal here is to reduce dimensions
     # take transpose so that the shape is the same as the original data
     # do batch processing
@@ -98,73 +99,32 @@ def pca_with_batch_processing(data, batch_size=10):
         Z[i:batch_end] = (U_reduced.T @ batch).T
     return Z # is a tensor
 
-def process_data():
-    # todo: remove if not needed
-    number_of_rows_to_select_randomly = 200  # Need to cut down on data samples as data test set is too large
+# np.save('COVIDData', image_data) # saves data
 
-    P_data = process_images(P_folder, target_size)
-    number_of_data_points_for_P = P_data.shape[0]
-    random_indices_P = np.random.choice(number_of_data_points_for_P, number_of_rows_to_select_randomly, replace=False)
-    P_data = P_data[random_indices_P]
-    print("Pneumonia data has been processed.")
+# check_data = np.load('NormalData.npy') # for loading data
 
-    normal_data = process_images(NORMAL_folder_1, target_size)
-    number_of_data_points_for_NORMAL = normal_data.shape[0]
-    random_indices_Normal = np.random.choice(number_of_data_points_for_NORMAL, number_of_rows_to_select_randomly,
-                                             replace=False)
-    normal_data = normal_data[random_indices_Normal]
-    print("Normal data has been processed.")
+def process_data_in_batches(raw_data_1, raw_data_2, sample_size=200):
+    '''''''''
+    Takes in 2 matrices, each of which is a numpy array of shape (num data points, num features). Note that num columns 
+    must be the same for each matrix.
+    
+    Procedure:
+    1. Stack raw_data_1 on top of raw_data_2
+    2. 
+    '''''''''''
+    number_of_data_points_data_1 = raw_data_1.shape[0]
+    number_of_data_points_data_2 = raw_data_2.shape[0]
 
-    # join the data together by stacking the P_data on top of the normal
-    combined_data = np.vstack((P_data, normal_data))
-    print("Data has been combined.")
+    number_of_features = raw_data_1.shape[1]
 
-    # perform pca to reduce dimensionality of data
-    reduced_combined_data = pca(combined_data)
-    print("PCA complete")
-
-    # add in the output column
-    # 0 if normal and 1 if pneumonia is present
-    number_of_pneumonia_samples = P_data.shape[0]
-    ones_col = np.ones(number_of_pneumonia_samples)
-    number_of_normal_samples = normal_data.shape[0]
-    total_samples = number_of_pneumonia_samples + number_of_normal_samples
-    zeros_col = np.zeros(number_of_normal_samples)
-    output_col = np.vstack((ones_col, zeros_col))
-    processed_data = np.hstack((reduced_combined_data, output_col))
-    bias_col = np.ones(total_samples)
-    bias_col = np.reshape(bias_col, (total_samples, 1))
-    processed_data = np.hstack((bias_col, processed_data))
-    print("Data has been processed.")
-
-    # shuffle the data
-    np.random.seed(42)
-    np.random.shuffle(processed_data)
-
-    # save the data that has been processed as csv file
-    np.save('./Processed Data/PvNormalData', processed_data)
-    print("Data has been shuffled and saved.")
-
-    # np.save('COVIDData', image_data) # saves data
-
-    # check_data = np.load('NormalData.npy') # for loading data
-
-def process_data_in_batches(folder_1, folder_2, sample_size=200):
-    processed_data_1 = process_images(folder_1, target_size)
-    number_of_data_points_data_1 = processed_data_1.shape[0]
-    processed_data_2 = process_images(folder_2, target_size)
-    number_of_data_points_data_2 = processed_data_2.shape[0]
-
-    number_of_features = 65536
-
-    # reduce data size
+    # reduce data size by randomly picking data points, since original data set is quite large
     np.random.seed(46)
     random_indices = np.random.choice(number_of_data_points_data_1, sample_size, replace=False)
-    data_1 = processed_data_1[random_indices] # has shape: (sample_size, 65536)
+    data_1 = raw_data_1[random_indices] # has shape: (sample_size, num_features)
     random_indices = np.random.choice(number_of_data_points_data_2, sample_size, replace=False)
-    data_2 = processed_data_2[random_indices]
+    data_2 = raw_data_2[random_indices]
     # stack data_1 on TOP of data_2
-    joined_data = np.vstack((data_1, data_2)) # has shape: (2*sample_size, 65536) ~ (400, 65536) if sample size is 200
+    joined_data = np.vstack((data_1, data_2)) # has shape: (2*sample_size, num_features)
 
     # process data_1
     data_chunks = []
@@ -173,7 +133,7 @@ def process_data_in_batches(folder_1, folder_2, sample_size=200):
         # pick 64 * 64 columns at one time
         num_columns_to_pick = 64 * 64
         if i + num_columns_to_pick > number_of_features:
-            data_to_add = joined_data[i:number_of_features]
+            data_to_add = joined_data[:, i : number_of_features]
         else:
             data_to_add = joined_data[:, i : i + num_columns_to_pick]
         data_chunks.append(data_to_add)
@@ -181,7 +141,8 @@ def process_data_in_batches(folder_1, folder_2, sample_size=200):
 
     processed_data = None
     for data_chunk in data_chunks:
-        processed_data_chunk = pca(data_chunk)
+        #processed_data_chunk = pca(data_chunk)
+        processed_data_chunk = pca_with_batch_processing(data_chunk) # faster with batch processing
         if processed_data is None:
             processed_data = processed_data_chunk
         else:
@@ -202,9 +163,13 @@ def process_data_in_batches(folder_1, folder_2, sample_size=200):
     label_col = torch.vstack((zeros_col, ones_col))
     processed_data = np.hstack((processed_data, label_col))
 
-    #todo: save the data
+    return processed_data
 
+raw_data_1 = process_images(P_folder, target_size)
+raw_data_2 = process_images(NORMAL_folder_1, target_size)
+# if pneumonia present, label is 0
+PvNormalData = process_data_in_batches(raw_data_1, raw_data_2)
 
-#process_data_in_batches(P_folder, NORMAL_folder_1)
+#todo: save the data to a file
 
-#process_data()
+torch.save(PvNormalData, "./PVNormalData.pt")
