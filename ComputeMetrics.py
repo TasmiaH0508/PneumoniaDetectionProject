e845421fa1c2080831b87093ee38c0cdfb20eefe
TasmiaH0508 <Tasmiahaque2004@gmail.com>
2024-12-16 23:13:56 +0800

    Working on NN with torch

--- ComputeMetrics.py
+++ ComputeMetrics.py
@@ -1,1 +1,20 @@
-def accuracy():
+import torch
+import numpy as np
+
+def get_accuracy(actual_labels, predicted_labels):
+    ''''''''''
+    Takes in actual labels, which is a torch array of shape: (number of samples, 1) and 
+    predicted labels, which is a torch array of shape: (number of samples, 1)
+    
+    Note: Labels are 0 or 1 (Binary Classification)
+    
+    Returns percentage of correctly classified samples
+    '''''
+    total_labels = actual_labels.shape[0]
+
+    actual_labels = torch.reshape(actual_labels, (total_labels,))
+    predicted_labels = torch.reshape(predicted_labels, (total_labels,))
+
+    number_of_correctly_classified_points = torch.where(actual_labels == predicted_labels)[0].shape[0]
+
+    return number_of_correctly_classified_points / total_labels * 100
--- NeuralNetwork/LogisticRegressionNN.py
+++ NeuralNetwork/LogisticRegressionNN.py
@@ -19,3 +19,47 @@ print(f"Using {device} device")
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
+        self.L1 = nn.Linear(1935, 968, bias=False)
+        self.L2 = nn.LeakyReLU(negative_slope=0.2)
+        self.L3 = nn.Linear(968, 242)
+        self.L4 = nn.LeakyReLU(negative_slope=0.2)
+        self.L5 = nn.Linear(242, 96)
+        self.L6 = nn.LeakyReLU(negative_slope=0.2)
+        self.L7 = nn.Linear(96, 24)
+        self.L8 = nn.LeakyReLU(negative_slope=0.2)
+        self.L9 = nn.Linear(24, 1)
+        self.L10 = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.L1(x)
+        x = self.L2(x)
+        x = self.L3(x)
+        x = self.L4(x)
+        x = self.L5(x)
+        x = self.L6(x)
+        x = self.L7(x)
+        x = self.L8(x)
+        x = self.L9(x)
+        x = self.L10(x)
+        return x
+
+#model = NeuralNetwork().to(device)
+#print(model)
+
+data = np.load("../ProcessedData/PvNormalDataNormalised.npy")
+data = torch.from_numpy(data) # has shape (400, 1936), where the last col is the label
+data_without_label = data[:, 0: 1935]
+label = data[:, 1935]
+
+def train_model(epochs, data, label):
+    '''''''''
+    Input:
+    - data excludes the labels 
+    - labels contains the 
+    Returns weights of the trained model
+    '''''''''''
+    loss = float('inf')
+    optimiser =
+    loss_function = nn.BCELoss()
+    for i in range(epochs):
+        #todo tmrw
