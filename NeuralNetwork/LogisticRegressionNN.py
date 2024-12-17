602303e93be639598347f68d1c03b7096e25f1ef
TasmiaH0508 <Tasmiahaque2004@gmail.com>
2024-12-17 20:41:38 +0800

    Working on testing for LogisticRegressionNN

--- NeuralNetwork/LogisticRegressionNN.py
+++ NeuralNetwork/LogisticRegressionNN.py
@@ -1,7 +1,7 @@
-import os
 import torch
 from torch import nn
 import numpy as np
+from ComputeMetrics import get_accuracy
 
 data = np.load("../ProcessedData/PvNormalDataNormalised.npy")
 data = torch.from_numpy(data)
@@ -23,12 +23,10 @@ class NeuralNetwork(nn.Module):
         self.L2 = nn.LeakyReLU(negative_slope=0.2)
         self.L3 = nn.Linear(968, 242)
         self.L4 = nn.LeakyReLU(negative_slope=0.2)
-        self.L5 = nn.Linear(242, 96)
+        self.L5 = nn.Linear(242, 24)
         self.L6 = nn.LeakyReLU(negative_slope=0.2)
-        self.L7 = nn.Linear(96, 24)
-        self.L8 = nn.LeakyReLU(negative_slope=0.2)
-        self.L9 = nn.Linear(24, 1)
-        self.L10 = nn.Sigmoid()
+        self.L7 = nn.Linear(24, 1)
+        self.L8 = nn.Sigmoid()
 
     def forward(self, x):
         x = self.L1(x)
@@ -39,27 +37,51 @@ class NeuralNetwork(nn.Module):
         x = self.L6(x)
         x = self.L7(x)
         x = self.L8(x)
-        x = self.L9(x)
-        x = self.L10(x)
         return x
 
-#model = NeuralNetwork().to(device)
-#print(model)
+# Process data first
 
 data = np.load("../ProcessedData/PvNormalDataNormalised.npy")
 data = torch.from_numpy(data) # has shape (400, 1936), where the last col is the label
 data_without_label = data[:, 0: 1935]
 label = data[:, 1935]
+number_of_data_points = label.shape[0]
+label = torch.reshape(label, (label.shape[0], 1))
 
-def train_model(epochs, data, label):
+model = NeuralNetwork()
+
+def train_model(epochs, data_without_label, optimiser, save_weights=True, use_old_weights=False):
     '''''''''
-    Input:
-    - data excludes the labels 
-    - labels contains the 
-    Returns weights of the trained model
-    '''''''''''
-    loss = float('inf')
-    optimiser =
-    loss_function = nn.BCELoss()
+    Trains neural network with given parameters.
+    
+    If save_weights=False, the weights will not be saved to an external file and the weights generated cannot be 
+    used in future to train model further.
+    
+    If use_old_weights=False, the model will not use the weights previously generated for learning
+    '''
+    if use_old_weights:
+        state_dict = torch.load("./torch_weights.pth", weights_only=True)
+        model.load_state_dict(state_dict)
+
+    loss_fn = nn.BCELoss()
     for i in range(epochs):
-        #todo tmrw
+        optimiser.zero_grad()
+        y_pred = model(data_without_label)
+        loss = loss_fn(y_pred, label)
+        loss.backward()
+        optimiser.step()
+
+    if save_weights:
+        weights = model.state_dict()
+        torch.save(weights, "./torch_weights.pth")
+
+def predict(data_without_label, threshold_probability):
+    model.load_state_dict(torch.load("./torch_weights.pth", weights_only=True))
+    model.eval()
+    predictions = model(data_without_label)
+    print(predictions)
+    return 0
+
+optimiser = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0)
+#train_model(50, data_without_label, optimiser, save_weights=True, use_old_weights=False)
+predict(data_without_label, 0.5)
