021ad11db898b97af52465a3d10e20215aa4f2b1
TasmiaH0508 <Tasmiahaque2004@gmail.com>
2024-12-31 10:45:19 +0300

    Testing and debugging

--- LogisticRegressionWONeuralNet/LogisticRegression_wo_NN.py
+++ LogisticRegressionWONeuralNet/LogisticRegression_wo_NN.py
@@ -6,7 +6,7 @@ import time
 class LargeDataSizeError(Exception):
     pass
 
-large_feature_num = 2400
+large_feature_num = 2400 # this number can be adjusted to be a higher number
 
 def transform_features(data, poly_deg=2):
     ''''
@@ -17,6 +17,7 @@ def transform_features(data, poly_deg=2):
 
     For e.g., [u, v] -> [u, v, u^2, u.v, v^2], for poly_deg=2
     '''
+    # code kept for testing purposes
     pow_to_range = dict() # stores the ranges at which linear, quadratic, cubic terms are present...
     pow_to_range[1] = range(0, data.shape[1])
     has_been_computed = set()
@@ -43,10 +44,9 @@ def transform_features(data, poly_deg=2):
             pow_to_range[deg_i] = range(end_of_range_of_deg_preceding_deg_i, num_features)
     return data
 
-def transform_features_batch_processing(data, poly_deg=2, batch_size=30):
+def transform_features_with_batch_processing(data, poly_deg=2, batch_size=600):
     pow_to_range = dict()  # stores the ranges at which linear, quadratic, cubic terms are present...
     pow_to_range[1] = range(0, data.shape[1])
-    has_been_computed = set()
     if poly_deg >= 2:
         # by the end of every iteration of the outer loop, (transformed) features of deg_i are added.
         for deg_i in range(2, poly_deg + 1):
@@ -55,48 +55,41 @@ def transform_features_batch_processing(data, poly_deg=2, batch_size=30):
             while low_deg <= high_deg:
                 range_of_low_deg = pow_to_range[low_deg]
                 range_of_high_deg = pow_to_range[high_deg]
-                # double loop combines 2 single features of low_deg and high_deg together
+                # combines 2 single features of low_deg and high_deg together
                 for i in range_of_low_deg:
-                    for j in range_of_high_deg:
-                        if (j, i) not in has_been_computed and (i, j) not in has_been_computed:
-                            col_to_add = data[:, i] * data[:, j]
-                            col_to_add = torch.reshape(col_to_add, (col_to_add.shape[0], 1))
-                            data = torch.hstack((data, col_to_add))
-                            has_been_computed.add((i, j))
+                    col_to_multiply = data[:, i]
+                    col_to_multiply = torch.reshape(col_to_multiply, (col_to_multiply.shape[0], 1))
+                    cols_needed = torch.arange(range_of_high_deg.start, range_of_high_deg.stop)
+                    mat = data[:, cols_needed] # mat may be very large
+                    num_cols = mat.shape[1]
+                    cols_to_add = None
+                    # this loop applies batch processing to mat * col_to_multiply, as mat may be very large
+                    for j in range(0, num_cols, batch_size):
+                        batch_end = min(j + batch_size, num_cols)
+                        mat_segment = mat[:, j : batch_end]
+                        segment_to_add = mat_segment * col_to_multiply
+                        if cols_to_add is None:
+                            cols_to_add = segment_to_add
+                        else:
+                            cols_to_add = torch.hstack((cols_to_add, segment_to_add))
+                    print("Taking feature: " + str(i))
+                    print(cols_to_add)
+                    if i == 1 and low_deg == high_deg == 1:
+                        # todo: debug; this needs debugging since there is a double counting of features
+                        cols_to_add = cols_to_add[:, 1]
+                        cols_to_add = torch.reshape(cols_to_add, (cols_to_add.shape[0], 1))
+                    data = torch.hstack((data, cols_to_add))
                 low_deg += 1
                 high_deg -= 1
+                print("End of one iteration of while loop")
             end_of_range_of_deg_preceding_deg_i = pow_to_range[deg_i - 1].stop
             num_features = data.shape[1]
             pow_to_range[deg_i] = range(end_of_range_of_deg_preceding_deg_i, num_features)
     return data
 
-data = np.load("../ProcessedRawData/TrainingSet/PvNormalDataNormalised_var0.06.npy")
-data = torch.from_numpy(data)
-
-def get_new_quadratic_features(data_1, data_2):
-    ''''
-    data_1 and data_2 are torch tensors, both of which have the exact same size.
-
-    Returns the new quadratic features.
-
-    Suitable for large data sets.
-    '''
-    num_data_pts = data_1.shape[0]
-    num_features_1 = data_1.shape[1]
-    num_features_2 = data_2.shape[1]
-    num_new_features = num_features_1 * num_features_2
-    collected_features = torch.zeros((num_data_pts, num_new_features))
-    index = 0
-    for i in range(num_features_1):
-        for j in range(num_features_2):
-            col_to_add = data_1[:, i] * data_2[:, j]
-            collected_features[:, index] = col_to_add
-            index += 1
-    return collected_features
-
-def train_model(epochs, train_data, has_bias=True, poly_deg=1, lr=0.001, gradient_descent_type='SGD'):
+def train_model(epochs, train_data, has_bias=True, poly_deg=1, lr=0.001, gradient_descent_type='SGD', batch_size=200):
     """""""""
-    Data must have the label col. Uses sgd
+    Data must have the label col. 
     """
     actual_label = get_label(train_data)
     num_features = train_data.shape[1]
@@ -106,11 +99,11 @@ def train_model(epochs, train_data, has_bias=True, poly_deg=1, lr=0.001, gradien
         if num_features > large_feature_num and poly_deg > 1:
             raise LargeDataSizeError
     except LargeDataSizeError:
-        print("If you want to continue using the same dataset, set poly_deg=1.")
+        print("If you want to continue using the same dataset, set poly_deg=1 or some other smaller number.")
 
     # transform the features
     data_wo_bias_and_label = get_data_without_bias_and_label(train_data, has_bias=has_bias, has_label=True)
-    transformed_data = transform_features(data_wo_bias_and_label, poly_deg=poly_deg)
+    transformed_data = transform_features_with_batch_processing(data_wo_bias_and_label, poly_deg=poly_deg, batch_size=batch_size)
 
     # add back bias col
     transformed_data = add_bias(transformed_data)
@@ -121,10 +114,10 @@ def train_model(epochs, train_data, has_bias=True, poly_deg=1, lr=0.001, gradien
 
     np.random.seed(32)
     # train the model
-    if gradient_descent_type == 'standard':
+    if gradient_descent_type == 'BGD':
         #todo
         weights = 0
-    elif gradient_descent_type == 'BGD':
+    elif gradient_descent_type == 'mBGD':
         #todo
         weights = 0
     else:
@@ -133,6 +126,9 @@ def train_model(epochs, train_data, has_bias=True, poly_deg=1, lr=0.001, gradien
     return weights
 
 def get_weights_stochastic_gradient_descent(epochs, data_wo_label, label, weights, lr):
+    ''''
+
+    '''
     num_data_pts = data_wo_label.shape[0]
     for i in range(epochs):
         random_index = np.random.choice(num_data_pts, size=1, replace=False)[0]
@@ -141,12 +137,10 @@ def get_weights_stochastic_gradient_descent(epochs, data_wo_label, label, weight
         actual_label_of_data_pt = label[random_index]
         partial_derivative_of_loss_wrt_weights = data_pt * (h_w_of_data_pt - actual_label_of_data_pt)
         weights = weights - lr * partial_derivative_of_loss_wrt_weights
-        if i == epochs - 1:
-            print(weights)
     return weights
 
 def get_weights_batch_gradient_descent(epochs, data_wo_label, label, weights, lr):
-    #todo
+
     return 0
 
 def compute_loss():
@@ -158,13 +152,6 @@ def h_w(weights, x):
     x = 1 / (1 + np.exp(-x))
     return x
 
-"""""""""
-start = time.time()
-train_quadratic_model(1, data)
-end = time.time()
-print("Process ended in ", end - start, "s") # 90s
-"""
-
 def add_bias(data):
     num_data_pts = data.shape[0]
     bias_col = torch.ones((num_data_pts, 1))
@@ -172,9 +159,6 @@ def add_bias(data):
     return data
 
 def predict(test_data, weights, has_label=True, has_bias=True, probability_threshold=0.5, poly_deg=1):
-    # todo: throw an error if data size is too big and if poly_deg = 2
-
-    # todo: also add in a way for poly_deg=2 to be able to call the relevant method with batch processing
     test_data = get_data_without_bias_and_label(test_data, has_bias=has_bias, has_label=has_label)
     test_data = transform_features(test_data, poly_deg=poly_deg)
     test_data = add_bias(test_data)
--- LogisticRegressionWONeuralNet/Test.py
+++ LogisticRegressionWONeuralNet/Test.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from LogisticRegression_wo_NN import *
+import time
 
 # decision boundary = x^2 + 2xy
 # if x^2 + 2xy > 0, 1 else 0
@@ -11,22 +12,15 @@ train = torch.asarray([[0, 1, 0],
                       [2, 0, 1],
                       [0, 2, 0]]) # the first 2 cols are features
 
-test = torch.asarray([[0, 3, 0],
-                      [3, 0, 1],
-                      [2, 4, 1]])
+test = torch.asarray([[0, 3],
+                      [3, 0]])
 
 #weights = train_model(10, train, has_bias=False, poly_deg=2, lr=0.01)
 #print(predict(test, weights, has_label=True, has_bias=False, poly_deg=2))
 
-test_1 = torch.asarray([[1, 2],
-                      [2, 4]])
-#print(test_1)
+testing = torch.asarray([[0, 1, 0],
+                         [1, 0, 1]])
 
-res = transform_features(test_1, poly_deg=3)
-#print(res)
+#print(transform_features(testing))
 
-test_1_up_to_quadratic_term = transform_features(test_1, poly_deg=2)
-print(test_1_up_to_quadratic_term)
-
-quadratic_features = get_new_quadratic_features(test_1, test_1)
-print("The quadratic terms are: \n", quadratic_features)
+print(transform_features_with_batch_processing(testing, batch_size=1))
