7a1134b40181a7065ff1c2083d663f1b14dc8d42
TasmiaH0508 <Tasmiahaque2004@gmail.com>
2025-06-22 21:21:22 +0800

    Working on CNN model

--- App/Models/CNN/CNN.py
+++ App/Models/CNN/CNN.py
@@ -11,29 +11,16 @@ print(device)
 
 target_size = (256, 256)
 
-transform_to_use = transforms.Compose([torchvision.transforms.Grayscale(),
+transform_to_use = transforms.Compose([torchvision.transforms.Grayscale(num_output_channels=1),
                                 torchvision.transforms.RandomHorizontalFlip(p=0.5),
                                 torchvision.transforms.Resize(target_size),
                                 torchvision.transforms.ToTensor()])
 
 class CNN(nn.Module):
-    # need to fix this
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
         return x
 
 def prepare_data_loader(transform=transform_to_use, path_to_training_data='../Data/CNNData/Train',
