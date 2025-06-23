a93c64697f6e6734602f6ec88e5a12bde793fcf8
TasmiaH0508 <Tasmiahaque2004@gmail.com>
2025-06-23 23:03:38 +0800

    Debug architecture

--- App/Models/CNN/CNN.py
+++ App/Models/CNN/CNN.py
@@ -1,7 +1,6 @@
 import torch
 import torchvision
 from torch import nn
-import torch.nn.functional as F
 from torchvision.transforms import transforms
 import torch.optim as optim
 
@@ -11,16 +10,43 @@ print(device)
 
 target_size = (256, 256)
 
-transform_to_use = transforms.Compose([torchvision.transforms.Grayscale(num_output_channels=1),
-                                torchvision.transforms.RandomHorizontalFlip(p=0.5),
+transform_to_use = transforms.Compose([torchvision.transforms.Grayscale(num_output_channels=3),
+                                # torchvision.transforms.RandomHorizontalFlip(p=0.5),
                                 torchvision.transforms.Resize(target_size),
                                 torchvision.transforms.ToTensor()])
 
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
+        torch.random.manual_seed(0)
+        self.L1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.L2 = nn.MaxPool2d(2, 2)
+
+        self.L3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.L4 = nn.MaxPool2d(2, 2)
+
+        self.L5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.L6 = nn.MaxPool2d(2, 2)
+
+        self.L7 = nn.Linear(128 * 32 * 32, 128)
+        self.L8 = nn.Dropout(0.5)
+        self.L9 = nn.Linear(128, 1)
 
     def forward(self, x):
+        x = self.L1(x)
+        x = self.L2(x)
+        x = self.L3(x)
+        x = self.L4(x)
+        x = self.L5(x)
+        print(x.shape) # batch_size=32, input_channels=128, height=64, width=64
+        x = self.L6(x)
+        print(x.shape) # batch_size=32, input_channels=128, height=32, width=32
+        x = self.L7(x)
+        print(x.shape)
+        x = self.L8(x)
+        print(x.shape)
+        x = self.L9(x)
+        print(x.shape)
         return x
 
 def prepare_data_loader(transform=transform_to_use, path_to_training_data='../Data/CNNData/Train',
@@ -35,34 +61,28 @@ def prepare_data_loader(transform=transform_to_use, path_to_training_data='../Da
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
     return training_loader, validation_loader, test_loader
 
-def train():
-    #t odo
+def train(epochs):
     net = CNN()
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCELoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    trainloader, valloader, testloader = prepare_data_loader()
+    train_loader, val_loader, test_loader = prepare_data_loader()
 
-    for epoch in range(2):  # loop over the dataset multiple times
-
+    for epoch in range(epochs):
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
+        for i, data in enumerate(train_loader, 0):
             inputs, labels = data
 
-            # zero the parameter gradients
             optimizer.zero_grad()
 
-            # forward + backward + optimize
             outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
-            # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
+            if i % 2000 == 1999:
                 print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
                 running_loss = 0.0
 
